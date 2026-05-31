e921659e17a8452e09cf34646a587b95ddf4c262
AliceTseng0810 <yexiu07060810@gmail.com>
2026-05-31 14:35:37 +0200

    update

--- auto-push.bat
+++ auto-push.bat
@@ -1,4 +1,13 @@
-@echo off  
-git add .  
-git commit -m "Auto-update: File changed"  
-git push origin main 
+@echo off
+:loop
+git add .
+:: 檢查有沒有檔案被修改，有的話才送出
+git diff-index --quiet HEAD --
+if %errorlevel% neq 0 (
+    git commit -m "Auto-update: File changed"
+    git push origin main
+    echo [OK] 偵測到網頁更新，已自動同步！
+)
+:: 每一百秒（100秒）自動檢查一次，你可以自行調整數字
+timeout /t 30> nul
+goto loop
